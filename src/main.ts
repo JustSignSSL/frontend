19c526e7ac4639169a0125d0ce6025b0eeb232af
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-08-12 08:41:09 +0800

    feat: tree shaking for Element-Plus

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from 'pinia'
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 import { i18n } from './util/i18n'
 
 createApp(App)
-    .use(ElementPlus)
     .use(createPinia())
     .use(i18n)
     .mount("#app");
